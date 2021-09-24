041a5732134a26aad52a1a237997a4e9ec8c72e8
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-24 14:37:06 -0300

    lesson: 39 create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss'
+export const Header = () => {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/logo.svg" alt="DevNews!" />
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
