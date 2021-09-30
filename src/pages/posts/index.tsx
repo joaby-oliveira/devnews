e262d81d440541e0ba9ee140ed5eb943073ba138
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-30 11:25:58 -0300

    lesson: 45 - Static Site Generation 1

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,36 @@
-const index = () => {
+import { GetStaticProps } from "next"
+
+interface Post {
+  id: string,
+  title: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+const Posts = ({ posts }: PostsProps) => {
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default index
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts')
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
+
+export default Posts
